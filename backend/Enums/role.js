90ba406cee137a951778b6ad788ea38713bdeb94
xHeptc <mantasbakys123@gmail.com>
2026-03-18 11:17:17 +0200

    commit

---
+++ backend/Enums/role.js
@@ -0,0 +1,4 @@
+export default Enums = {
+     USER: 'user',
+     ADMIN: 'admin'
+}
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,12 @@
 import mongoose from "mongoose"
+import role from "..//role.js"
 
 const UserSchema = mongoose.Schema({
-     
-})
+     username: { type: String, min: 3, required: true },
+     email: { type: String, unique: true, required: true },
+     password: { type: String, required: true },
+     role: { type: String, default: role.USER }
+}, { timestamps: true })
+
+const UserModel = mongoose.model(UserSchema)
+export default UserModel
